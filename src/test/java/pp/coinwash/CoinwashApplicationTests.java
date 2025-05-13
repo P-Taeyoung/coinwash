e2ef133540e8798adbc8e50976e6ce7593ee0479
pty10510 <pty10510@gmail.com>
2025-05-13 23:14:18 +0900

    Initial commit

---
+++ src/test/java/pp/coinwash/CoinwashApplicationTests.java
@@ -0,0 +1,13 @@
+package pp.coinwash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinwashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
