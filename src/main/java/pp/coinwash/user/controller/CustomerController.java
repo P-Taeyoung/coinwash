c87fa0e4508aa1497b266dab2076c44337af0eec
pty10510 <pty10510@gmail.com>
2025-05-14 22:41:11 +0900

    feat : 고객 CRUD 기능 생성

---
+++ src/main/java/pp/coinwash/user/controller/CustomerController.java
@@ -0,0 +1,50 @@
+package pp.coinwash.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import pp.coinwash.user.domain.dto.CustomerResponseDto;
+import pp.coinwash.user.domain.dto.CustomerSignUpDto;
+import pp.coinwash.user.domain.dto.CustomerUpdateDto;
+import pp.coinwash.user.service.CustomerService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customer")
+public class CustomerController {
+
+	private final CustomerService customerService;
+
+	@PostMapping
+	public ResponseEntity<String> saveCustomer(@RequestBody CustomerSignUpDto dto) {
+		customerService.signUp(dto);
+		return ResponseEntity.ok("회원가입이 완료되었습니다.");
+	}
+
+	@GetMapping
+	//TODO 추후 ContextHolder 에서 고객id 를 가져올 수 있도록
+	public ResponseEntity<CustomerResponseDto> getCustomers(@RequestParam long customerId) {
+		return ResponseEntity.ok(customerService.getCustomer(customerId));
+	}
+
+	@PatchMapping
+	public ResponseEntity<String> updateCustomer(@RequestParam long customerId
+		,@RequestBody CustomerUpdateDto dto) {
+		customerService.updateCustomer(customerId, dto);
+		return ResponseEntity.ok("회원정보가 수정되었습니다.");
+	}
+
+	@DeleteMapping
+	public ResponseEntity<String> deleteCustomer(@RequestParam long customerId) {
+		customerService.deleteCustomer(customerId);
+		return ResponseEntity.ok("회원 탈퇴되었습니다.");
+	}
+}
---
+++ src/main/java/pp/coinwash/user/service/CustomerService.java
@@ -0,0 +1,48 @@
+package pp.coinwash.user.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import pp.coinwash.user.domain.dto.CustomerResponseDto;
+import pp.coinwash.user.domain.dto.CustomerSignUpDto;
+import pp.coinwash.user.domain.dto.CustomerUpdateDto;
+import pp.coinwash.user.domain.entity.Customer;
+import pp.coinwash.user.domain.repository.CustomerRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+	private final CustomerRepository customerRepository;
+
+	public void signUp(CustomerSignUpDto dto) {
+		validateId(dto.id());
+		customerRepository.save(Customer.of(dto));
+	}
+
+	public CustomerResponseDto getCustomer(long customerId) {
+		return CustomerResponseDto.from(validateCustomer(customerId));
+	}
+
+	@Transactional
+	public void updateCustomer(long customerId, CustomerUpdateDto dto) {
+		validateCustomer(customerId).update(dto);
+	}
+
+	@Transactional
+	public void deleteCustomer(long customerId) {
+		validateCustomer(customerId).delete();
+	}
+
+	private void validateId(String Id) {
+		if (customerRepository.existsById(Id)) {
+			throw new RuntimeException("이미 존재하는 아이디입니다.");
+		}
+	}
+
+	private Customer validateCustomer(long customerId) {
+		return customerRepository.findById(customerId)
+			.orElseThrow(() -> new RuntimeException("회원 정보를 찾을 수 없습니다."));
+	}
+}
