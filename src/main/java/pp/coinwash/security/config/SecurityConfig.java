6b8cb1b7984fa98ff5cb86f62dd87afc8dbb8c1f
pty10510 <pty10510@gmail.com>
2025-05-20 21:48:59 +0900

    feat : Security 사용을 위한 Security 설정 코드 추가

---
+++ src/main/java/pp/coinwash/security/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package pp.coinwash.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import pp.coinwash.security.filter.JwtFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtFilter jwtFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(session ->
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		http
+			.authorizeHttpRequests(authorizeRequests ->
+				authorizeRequests
+					//TODO 허용할 경로 추가
+					.requestMatchers("/**")
+					.permitAll()
+					.anyRequest().authenticated());
+
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+}
