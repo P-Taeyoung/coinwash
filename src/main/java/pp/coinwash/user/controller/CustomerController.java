b3d123a150b4f062c0b15e07eef6d4408e4f88e2
pty10510 <pty10510@gmail.com>
2025-05-21 11:39:43 +0900

    refactor : 로그인 정보를 담는 dto는 점주, 고객 구분이 필요없기 떄문에 user로 통합

--- src/main/java/pp/coinwash/user/controller/CustomerController.java
+++ src/main/java/pp/coinwash/user/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import pp.coinwash.user.domain.dto.CustomerResponseDto;
-import pp.coinwash.user.domain.dto.CustomerSignInDto;
+import pp.coinwash.user.domain.dto.UserSignInDto;
 import pp.coinwash.user.domain.dto.CustomerSignUpDto;
 import pp.coinwash.user.domain.dto.CustomerUpdateDto;
 import pp.coinwash.user.service.CustomerService;
@@ -25,7 +25,7 @@ public class CustomerController {
 	private final CustomerService customerService;
 
 	@PostMapping("/signin")
-	public ResponseEntity<String> signIn(@RequestBody CustomerSignInDto dto) {
+	public ResponseEntity<String> signIn(@RequestBody UserSignInDto dto) {
 
 		return ResponseEntity.ok(customerService.signIn(dto));
 	}
