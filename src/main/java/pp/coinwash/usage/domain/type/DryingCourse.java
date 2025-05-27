45e8d96fdac588eb90bdc20e568f3a02c7904d85
pty10510 <pty10510@gmail.com>
2025-05-27 22:57:17 +0900

    feat : 세탁기, 건조기 사용을 구별하기 위한 WashingCourse, DryingCourse enum 클래스 추가

---
+++ src/main/java/pp/coinwash/usage/domain/type/DryingCourse.java
@@ -0,0 +1,14 @@
+package pp.coinwash.usage.domain.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum DryingCourse {
+	DRYING_A_COURSE(20),
+	DRYING_B_COURSE(30),
+	DRYING_C_COURSE(40);
+
+	final int courseTime;
+}
