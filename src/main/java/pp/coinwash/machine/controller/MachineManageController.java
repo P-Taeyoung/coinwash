6ee2cc76655e0fbdc38f75d56de839fcc2c4cd9d
pty10510 <pty10510@gmail.com>
2025-05-27 21:14:17 +0900

    fix : @RequestBody @RequestParam 을 통해 API 요청으로부터 원하는 매개변수 데이터를 가져올 수 있도록 함.

--- src/main/java/pp/coinwash/machine/controller/MachineManageController.java
+++ src/main/java/pp/coinwash/machine/controller/MachineManageController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -24,25 +26,34 @@ public class MachineManageController {
 	private final MachineManageService machineManageService;
 
 	@PostMapping
-	public ResponseEntity<String> registerMachine(List<MachineRegisterDto> dtos,long laundryId, long ownerId) {
+	public ResponseEntity<String> registerMachine(
+		@RequestBody List<MachineRegisterDto> dtos,
+		@RequestParam long laundryId,
+		@RequestParam long ownerId) {
 		machineManageService.registerMachines(dtos, laundryId, ownerId);
 
 		return ResponseEntity.ok("기계정보가 정상적으로 저장되었습니다.");
 	}
 
 	@GetMapping
-	public ResponseEntity<List<MachineResponseDto>> getMachines(long laundryId, long ownerId) {
+	public ResponseEntity<List<MachineResponseDto>> getMachines(
+		@RequestParam long laundryId,
+		@RequestParam long ownerId) {
 		return ResponseEntity.ok(machineManageService.getMachinesByLaundryId(laundryId, ownerId));
 	}
 
 	@PatchMapping
-	public ResponseEntity<String> updateMachine(MachineUpdateDto dto, long ownerId) {
+	public ResponseEntity<String> updateMachine(
+		@RequestBody MachineUpdateDto dto,
+		@RequestParam long ownerId) {
 		machineManageService.updateMachine(dto, ownerId);
 		return ResponseEntity.ok("기계정보가 정상적으로 수정되었습니다.");
 	}
 
 	@DeleteMapping
-	public ResponseEntity<String> deleteMachine(long laundryId, long ownerId) {
+	public ResponseEntity<String> deleteMachine(
+		@RequestParam long laundryId,
+		@RequestParam long ownerId) {
 		machineManageService.deleteMachine(laundryId, ownerId);
 		return ResponseEntity.ok("기계정보가 정상적으로 삭제되었습니다.");
 	}
