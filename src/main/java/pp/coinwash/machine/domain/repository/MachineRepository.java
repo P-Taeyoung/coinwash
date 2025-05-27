fd9a61f8da15dcdcf2456e56686031d1cbee98ea
pty10510 <pty10510@gmail.com>
2025-05-27 20:24:04 +0900

    feat : 세탁소 정보를 통한 기계 정보 조회를 위해 jpa 쿼리 추가

--- src/main/java/pp/coinwash/machine/domain/repository/MachineRepository.java
+++ src/main/java/pp/coinwash/machine/domain/repository/MachineRepository.java
@@ -1,8 +1,13 @@
 package pp.coinwash.machine.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pp.coinwash.machine.domain.entity.Machine;
 
 public interface MachineRepository extends JpaRepository<Machine, Long> {
+	List<Machine> findByLaundryLaundryId(long laundryId);
+	Optional<Machine> findByMachineIdAndLaundryOwnerId(long machineId, long ownerId);
 }
