886a6052731dec8abb2a318817a0d352e6693997
pty10510 <pty10510@gmail.com>
2025-05-28 14:22:35 +0900

    feat : 사용내역 조회 시 페이지 객체로 반환될 수 있도록 함

--- src/main/java/pp/coinwash/usage/domain/repository/UsageHistoryRepository.java
+++ src/main/java/pp/coinwash/usage/domain/repository/UsageHistoryRepository.java
@@ -1,8 +1,11 @@
 package pp.coinwash.usage.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pp.coinwash.usage.domain.entity.UsageHistory;
 
 public interface UsageHistoryRepository extends JpaRepository<UsageHistory, Long> {
+	Page<UsageHistory> findAllByCustomerId(long customerId, Pageable pageable);
 }
