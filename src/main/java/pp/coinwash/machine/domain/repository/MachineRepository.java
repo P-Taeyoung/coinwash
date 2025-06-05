3a7ded67045614f69cbbdd5d96d720fdf5c73902
pty10510 <pty10510@gmail.com>
2025-06-05 11:01:33 +0900

    feat : 기계 상태 변경을 위해 데이터 조회 시 비관적 락 적용

--- src/main/java/pp/coinwash/machine/domain/repository/MachineRepository.java
+++ src/main/java/pp/coinwash/machine/domain/repository/MachineRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import jakarta.persistence.LockModeType;
 import pp.coinwash.machine.domain.entity.Machine;
 import pp.coinwash.machine.domain.type.MachineType;
 
@@ -15,9 +17,11 @@ public interface MachineRepository extends JpaRepository<Machine, Long> {
 	Optional<Machine> findByMachineIdAndLaundryOwnerId(long machineId, long ownerId);
 
 	@Query("SELECT m FROM Machine m WHERE m.machineId = :machineId AND m.machineType = :machineTyp")
-	Optional<Machine> findMachineByMachineId(@Param("machineId") long machineId
+	@Lock(LockModeType.PESSIMISTIC_READ)
+	Optional<Machine> findUsableMachineWithLock(@Param("machineId") long machineId
 		, @Param("machineTyp") MachineType machineTyp);
 
 	@Query("SELECT m FROM Machine m WHERE m.machineId = :machineId AND m.usageStatus = 'USABLE'")
+	@Lock(LockModeType.PESSIMISTIC_READ)
 	Optional<Machine> findUsableMachineByMachineId(long machineId);
 }
