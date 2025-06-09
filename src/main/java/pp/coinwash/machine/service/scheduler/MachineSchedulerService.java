8be18828432b9055899ce6f869b36319b520369e
pty10510 <pty10510@gmail.com>
2025-06-09 22:24:17 +0900

    feat : 스케쥴러를 통해 사용/예약 종료 시 자동으로 기계 상테가 사용가능한 상태로 변경될 수 있도록 함.

---
+++ src/main/java/pp/coinwash/machine/service/scheduler/MachineSchedulerService.java
@@ -0,0 +1,58 @@
+package pp.coinwash.machine.service.scheduler;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import pp.coinwash.machine.domain.entity.Machine;
+import pp.coinwash.machine.service.UsingMachineService;
+import pp.coinwash.machine.service.redis.MachineRedisService;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MachineSchedulerService {
+
+	private final MachineRedisService machineRedisService;
+	private final UsingMachineService usingMachineService;
+	private final TaskScheduler taskScheduler;
+	private final Map<Long, ScheduledFuture<?>> scheduledTasks = new ConcurrentHashMap<>();
+
+	public void scheduleMachine(Machine machine, LocalDateTime endTime) {
+		//기존 스케줄이 있었다면 해당 스케줄 삭제
+		cancelScheduledTask(machine.getMachineId());
+
+		Instant instant = endTime.atZone(ZoneId.systemDefault()).toInstant();
+
+		ScheduledFuture<?> future = taskScheduler.schedule(() -> {
+
+			log.info("사용 가능한 상태로 변경된 기계 ID: {}", machine.getMachineId());
+
+			machineRedisService.resetMachine(machine);
+			usingMachineService.resetStatus(machine.getMachineId());
+
+			scheduledTasks.remove(machine.getMachineId());
+
+		}, instant);
+
+		scheduledTasks.put(machine.getMachineId(), future);
+		log.info("기계 스케줄링 완료: machineId={}, endTime={}", machine.getMachineId(), endTime);
+
+		scheduledTasks.put(machine.getMachineId(), future);
+	}
+
+	public void cancelScheduledTask(Long machineId) {
+		ScheduledFuture<?> future = scheduledTasks.remove(machineId);
+		if (future != null && !future.isDone()) {
+			future.cancel(false);
+		}
+	}
+}
