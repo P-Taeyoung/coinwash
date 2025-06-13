729fd6599ca2755fd521a6ffc449bdc6f1bb4246
pty10510 <pty10510@gmail.com>
2025-06-13 13:07:17 +0900

    feat : HistoryEvent 내 생성자 메서드 추가

--- src/main/java/pp/coinwash/history/event/HistoryEvent.java
+++ src/main/java/pp/coinwash/history/event/HistoryEvent.java
@@ -1,10 +1,18 @@
 package pp.coinwash.history.event;
 
+import lombok.Builder;
 import pp.coinwash.history.domain.dto.HistoryRequestDto;
 import pp.coinwash.machine.domain.entity.Machine;
 
+@Builder
 public record HistoryEvent(
 	HistoryRequestDto requestDto,
 	Machine machine
 ) {
+	public static HistoryEvent of(HistoryRequestDto requestDto, Machine machine) {
+		return HistoryEvent.builder()
+			.requestDto(requestDto)
+			.machine(machine)
+			.build();
+	}
 }
